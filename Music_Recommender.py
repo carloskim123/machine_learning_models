5e20e7b910f7750bf0f3ad1214bd1d6f1f4b64ed
carlos <115598773+maninthehood@users.noreply.github.com>
2023-06-26 22:05:05 -0400

    Update Music_Recommender.py

--- Music_Recommender.py
+++ Music_Recommender.py
@@ -30,7 +30,7 @@ score
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-music_data = pd.read_csv("../models/music.csv")
+music_data = pd.read_csv("music.csv")
 X = music_data.drop(columns=["genre"])
 y = music_data["genre"]
 
