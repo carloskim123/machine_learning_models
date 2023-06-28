a57373e03a478c23cff19220891a460b7af0d011
carlos <115598773+maninthehood@users.noreply.github.com>
2023-06-27 21:15:11 -0400

    Update Java Version.java

--- Java Version.java
+++ Java Version.java
@@ -1,73 +1,61 @@
+// This tests the model accuracy according to the amount of data its been given
+// example results: 0.5, 1.0, 1.5, 0.2223333
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import weka.classifiers.trees.J48;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
 
-public class MusicClassifier {
-    public static void main(String[] args) {
+public class MusicGenreClassifier {
+    public static void main(String[] args) throws Exception {
         // Load data
-        Instances data = loadData("music.csv");
+        Instances musicData = loadData("music.csv");
 
-        // Split data into features and target
-        data.setClassIndex(data.numAttributes() - 1);
-        Instances features = new Instances(data);
-        features.deleteAttributeAt(data.numAttributes() - 1);
+        // Split data into training and testing sets
+        Instances trainData = musicData.trainCV(5, 0);
+        Instances testData = musicData.testCV(5, 0);
 
-        // Create model and fit to data
-        J48 model = new J48();
-        try {
-            model.buildClassifier(data);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Create and build the classifier
+        Classifier classifier = new weka.classifiers.trees.J48();
+        classifier.buildClassifier(trainData);
 
-        // Make predictions
-        Instance instance1 = createInstance(features, 21, 1);
-        Instance instance2 = createInstance(features, 22, 0);
-        double[] preds;
-        try {
-            preds = model.distributionForInstance(instance1);
-            System.out.println("Prediction for instance 1: " + data.classAttribute().value((int) preds[0]));
-            preds = model.distributionForInstance(instance2);
-            System.out.println("Prediction for instance 2: " + data.classAttribute().value((int) preds[0]));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Evaluate the classifier
+        Evaluation eval = new Evaluation(trainData);
+        eval.evaluateModel(classifier, testData);
+
+        // Print the accuracy score
+        System.out.println(eval.pctCorrect());
     }
 
-    private static Instances loadData(String filename) {
-        BufferedReader reader;
-        Instances data = null;
-        try {
-            reader = new BufferedReader(new FileReader(filename));
-            data = new Instances(reader);
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return data;
-    }
-
-    private static Instance createInstance(Instances data, int age, int gender) {
+    private static Instances loadData(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
         List<Attribute> attributes = new ArrayList<>();
         attributes.add(new Attribute("age"));
         attributes.add(new Attribute("gender"));
+        attributes.add(new Attribute("genre", (List<String>) null));
 
-        Instances instances = new Instances("instance", attributes, 1);
-        instances.setClassIndex(data.numAttributes() - 1);
+        Instances data = new Instances("music_data", attributes, 0);
 
-        Instance instance = new DenseInstance(2);
-        instance.setDataset(instances);
-        instance.setValue(0, age);
-        instance.setValue(1, gender);
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split(",");
+            Instance instance = new DenseInstance(3);
+            instance.setValue(attributes.get(0), Double.parseDouble(values[0]));
+            instance.setValue(attributes.get(1), Double.parseDouble(values[1]));
+            instance.setValue(attributes.get(2), values[2]);
+            data.add(instance);
+        }
 
-        return instance;
+        reader.close();
+
+        return data;
     }
 }
